994107012636ebf8d1e8ef1e5625eec50e182ab1
magic10 <mostafa.udemy1@gmail.com>
2022-11-02 15:31:59 +0200

    added registration screen

---
+++ client_app/lib/presentation/screens/Registration/Registeration.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Registeration extends StatefulWidget {
+  const Registeration({super.key});
+
+  @override
+  State<Registeration> createState() => _RegisterationState();
+}
+
+class _RegisterationState extends State<Registeration> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
